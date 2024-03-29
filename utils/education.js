d64d63beeda0629bea52ec2e0697ae16eaefd000
Daniel Salvatierra Sanchez <147101731+DanielSalvatierraSanchez@users.noreply.github.com>
2024-03-29 18:16:37 +0100

    Add files via upload

--- utils/education.js
+++ utils/education.js
@@ -1,9 +1,9 @@
 export const education = [
-  { title: "Full Stack Developer", src: '../public/thePower.webp', },
-  { title: "Cisco Certified CCT Routing & Switching y Data Center", src: '../public/cctrs.png', },
-  { title: "Cisco Certified CCT Data Center", src: '../public/cctdc.png', },
+  { title: "Full Stack Developer", src: './thePower.webp', },
+  { title: "Cisco Certified CCT Routing & Switching y Data Center", src: './cctrs.webp', },
+  { title: "Cisco Certified CCT Data Center", src: './cctdc.webp', },
   { title: "Certified as Field Engineer of CISCO, AT&T, CIENA, VERITAS, NUTANIX, NVIDIA", 
-  src: '../public/network.webp',
+  src: './network.webp',
   },
-  { title: "Cice S.A. (Microsoft Certified Partner y A+ Certified as Technician of Computer Equipment and Network)", src: '../public/cice.webp', },
+  { title: "Cice S.A. (Microsoft Certified Partner y A+ Certified as Technician of Computer Equipment and Network)", src: './cice.webp', },
 ];
--- utils/experience.js
+++ utils/experience.js
@@ -1,10 +1,10 @@
 export const experience = {
-  cisco: '../public/cisco.webp',
-  cctrs: '../public/cctrs.webp',
-  cctdc: '../public/cctdc.webp',
-  ciena: '../public/ciena.webp',
-  att: '../public/att.webp',
-  veritas: '../public/veritas.webp',
-  nutanix: '../public/nutanix.webp',
-  nvidia: '../public/nvidia.webp',
+  cisco: './cisco.webp',
+  cctrs: './cctrs.webp',
+  cctdc: './cctdc.webp',
+  ciena: './ciena.webp',
+  att: './att.webp',
+  veritas: './veritas.webp',
+  nutanix: './nutanix.webp',
+  nvidia: './nvidia.webp',
 };
